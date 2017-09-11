b5a114b0754b6a5a200c9019e0a6d6c09f05bbc1
William Shiao <willshiao@gmail.com>
2017-09-11 15:47:38 -0700

    Create ProjectSerializer

---
+++ hts_site/management/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from management.models import Project
+
+
+class ProjectSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=30)
+    title = serializers.TextField()
+    date = serializers.DateField()
+    time = serializers.TimeField()
+    status = serializers.TextField()
+    name = serializers.CharField(max_length=100)
+    email = serializers.CharField(max_length=200)
+    phone = serializers.CharField(max_length=15)
+    pi = serializers.CharField(max_length=100)
+    billing_account = serializers.CharField(max_length=100)
+    department = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
+        return Project.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.title = validated_data.get('title', instance.title)
+        instance.date = validated_data.get('date', instance.date)
+        instance.time = validated_data.get('time', instance.time)
+        instance.status = validated_data.get('status', instance.status)
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.pi = validated_data.get('pi', instance.pi)
+        instance.billing_account = validated_data.get('billing_account', instance.billing_account)
+        instance.department = validated_data.get('department', instance.department)
+        instance.save()
+        return instance
